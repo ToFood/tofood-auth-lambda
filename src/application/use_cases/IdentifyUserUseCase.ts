d019e8209133ed0922b627dceebcf8b93f26602f
Robert A. dos Anjos <78178196+ohntrebor@users.noreply.github.com>
2024-10-01 20:38:08 -0300

    jwt

--- src/application/use_cases/IdentifyUserUseCase.ts
+++ src/application/use_cases/IdentifyUserUseCase.ts
@@ -1,4 +1,6 @@
 import { IUserRepository } from '../../core/interfaces/IUserRepository';
+import jwt from 'jsonwebtoken';
+import crypto from 'crypto'; // Biblioteca padrão para gerar valores aleatórios
 
 // Interface que define a requisição de identificação do usuário
 export interface IdentifyUserUseCaseRequest {
@@ -9,8 +11,8 @@ export interface IdentifyUserUseCaseRequest {
 export interface IdentifyUserUseCaseResponse {
     message: string; // Mensagem indicando o resultado da verificação
     statusCode: number; // Código HTTP adequado
+    token?: string; // Token de autenticação (se o usuário for encontrado)
 }
-
 
 // Caso de uso para identificar/verificar um usuário existente
 export class IdentifyUserUseCase {
@@ -29,9 +31,26 @@ export class IdentifyUserUseCase {
 
         // Retorna a resposta adequada de acordo com a presença do usuário no banco
         if (existingUser) {
+            // Geração do número aleatório e salt para incluir no payload do JWT
+            const randomNumber = Math.floor(Math.random() * 1000000); // Gera um número aleatório
+            const salt = crypto.randomBytes(16).toString('hex'); // Gera um salt aleatório
+
+            // Geração do token JWT
+            const token = jwt.sign(
+                {
+                    cpf: existingUser.cpf,
+                    name: existingUser.name,
+                    random: randomNumber,  // Inclui o número aleatório no payload
+                    salt: salt             // Inclui o salt no payload
+                },
+                process.env.JWT_SECRET || 'defaultsecret',
+                { expiresIn: '1h' } // Token válido por 1 hora
+            );
+
             return {
                 message: 'Cliente Identificado',
-                statusCode: 200
+                statusCode: 200,
+                token: token // Adiciona o token de autenticação na resposta
             };
         } else {
             return {
